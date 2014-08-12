7b800271f164f584717cc4f93123d8face76e8b9
Nathan Broadbent <nathan.f77@gmail.com>
2014-08-12 14:28:56 -0700

    Added script

---
+++ add_entries.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+$:.push File.expand_path("../lib", __FILE__)
+require 'mfp_scraper'
+require 'yaml'
+
+if ARGV.length != 1
+  puts "Usage: #{$0} \"MEAL: QUANTITY UNIT ITEM, ...\""
+  exit 1
+end
+
+if File.exist?('config.yml')
+  config = YAML.load_file('config.yml')
+else
+  puts "Please setup config.yml!"
+  exit 1
+end
+
+mfp = MFPScraper.new(username: config['username'], password: config['password'])
+mfp.add_food_entries_from_text(ARGV[0])
